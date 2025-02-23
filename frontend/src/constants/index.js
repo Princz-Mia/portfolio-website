b1260b3ef27d6b1749d88985a0b76693103d01bf
Princz Mia <princz.mia@gmail.com>
2025-02-23 21:43:40 +0100

    refactor: replace hardcoded navigation values with constants

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,22 @@
+export const navigation = [
+  {
+    id: "0",
+    title: "About Me",
+    url: "#about",
+  },
+  {
+    id: "1",
+    title: "Technologies",
+    url: "#technologies",
+  },
+  {
+    id: "2",
+    title: "Education",
+    url: "#education",
+  },
+  {
+    id: "3",
+    title: "Projects",
+    url: "#projects",
+  },
+];
