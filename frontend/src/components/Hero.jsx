7e479f67a4b0355b4e57a329c64b8cbc0cd7da3f
Princz Mia <princz.mia@gmail.com>
2025-02-23 21:35:35 +0100

    style: change in positioning and responsive settings

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,9 +5,9 @@ import { ReactSocialMediaIcons } from 'react-social-media-icons';
 
 const Hero = () => {
   const codeText = 
-  `// Hello there,
-var name = "Mia Princz";
-var spec = "Software Engineer";`;
+  `<h2>
+      Mia Princz, Software Engineer
+</h2>`;
 
   const iconVariants = {
     hover: {
@@ -18,21 +18,27 @@ var spec = "Software Engineer";`;
   };
 
   return (
-<section className="w-full h-auto pt-20 sm:pt-20 md:pt-12 px-4 flex flex-col md:flex-row items-center justify-center bg-radial from-gray-900 from-40% to-(--bg)">
-  <div className="w-full md:w-1/2 max-w-2xl flex flex-col p-6 mx-4 relative md:top-[-4em]">
-    <div className="bg-gray-900 text-(--cyan) p-6 shadow-lg border-2 border-(--blue) flex flex-col items-start">
-      <h2 className="font-mono text-sm md:text-lg xl:text-2xl whitespace-pre-wrap">
-        <Typewriter
-          words={[codeText]}
-          loop={1}
-          cursor
-          cursorStyle="|"
-          typeSpeed={50}
-          deleteSpeed={0}
-          delaySpeed={1000}
-        />
-      </h2>
-    </div>
+<section className="w-full min-h-screen pt-8 sm:pt-20 md:pt-12 px-4 flex flex-col items-center justify-center">
+  <div className="w-full max-w-2xl flex flex-col p-6 mx-4">
+  <div className="text-(--cyan) p-6 shadow-lg border-5 border-(--blue) flex flex-col items-start relative">
+  {/* Dummy elem: ez határozza meg a konténer méretét */}
+  <h2 className="font-(--font-jetbrains) text-sm md:text-lg xl:text-2xl whitespace-pre-wrap select-none visibility-hidden text-(--bg)">
+    {codeText}
+  </h2>
+  {/* Az animált Typewriter, amely abszolút helyezkedik el a dummy fölött */}
+  <h2 className="font-(--font-jetbrains)text-sm md:text-lg xl:text-2xl whitespace-pre-wrap absolute">
+    <Typewriter
+      words={[codeText]}
+      loop={1}
+      cursor
+      cursorStyle="|"
+      typeSpeed={50}
+      deleteSpeed={0}
+      delaySpeed={1000}
+    />
+  </h2>
+</div>
+
 
     <div className="flex justify-around w-full mt-12">
       {[{ icon: "mail", label: "Email", url: "https://example.com/email" },
@@ -48,9 +54,9 @@ var spec = "Software Engineer";`;
             borderColor="rgba(0,0,0,0.25)"
             icon={icon}
             iconColor="rgba(255,255,255,1)"
-            backgroundColor="rgba(26,166,233,1)"
+            backgroundColor="#569cfa"
             url={url}
-            size="64"
+            size="48"
           />
           <span className="absolute bottom-[-40px] text-sm text-gray-300 opacity-0 group-hover:opacity-100 transition-opacity">
             {label}
@@ -60,7 +66,7 @@ var spec = "Software Engineer";`;
     </div>
   </div>
 
-  <div className="w-full md:w-1/2 max-w-2xl flex items-center p-6 pt-12 md:pt-36 mx-4">
+  <div className="w-full md:w-2xl xl:w-3xl 2xl:w-4xl max-w-6xl flex items-center xl:pt-24 pt-12">
     <motion.div
       initial={{ translateY: 100, opacity: 0 }}
       animate={{ translateY: 0, opacity: 1 }}
