ebc508067f43b2ff93f5174587e5eb64e53ee9b8
Princz Mia <princz.mia@gmail.com>
2025-03-06 20:42:37 +0100

    chore: add string constants

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -20,3 +20,110 @@ export const navigation = [
     url: "#projects",
   },
 ];
+
+export const aboutMeParagraphs = [
+  {
+    id: "0",
+    content:
+      "Hi, I'm Mia Princz, a final-year Computer Engineering student currently pursuing my Bachelor's degree at the Budapest University of Technology and Economics. Specializing in software development at the Department of Automation and Applied Informatics, my primary focus and passion lie in backend development, though I'm also comfortable and capable when working on frontend projects.",
+  },
+  {
+    id: "1",
+    content:
+      "Throughout my academic journey, I've actively participated in diverse projects that allowed me to build robust software solutions, enhancing my skills in software development, database management, and API design. My experience includes technologies such as Java, Kotlin, .NET, various JavaScript frameworks and many more, enabling me to confidently handle end-to-end application development.",
+  },
+  {
+    id: "2",
+    content:
+      "I'm highly motivated, detail-oriented, and continuously eager to expand my professional knowledge and skills. My goal is to become part of an innovative team where I can contribute effectively while growing professionally and personally. I'm always open to exciting career opportunities and collaboration—let's connect and discuss how I can bring value to your organization!",
+  },
+];
+
+export const technologiesParagraph = `Throughout my previous projects and studies, I've gained hands-on experience and familiarity with a wide range of modern technologies. I'm continually expanding my knowledge by practicing and building new projects. I thrive in dynamic environments and I'm always eager to embrace new challenges, making me flexible and ready to quickly adapt to any technology stack required by the team or project.`;
+
+export const technologyCards = [
+  {
+    name: "Java",
+    description: "Object-oriented programming language.",
+    imageUrl: "/imgs/logos/java.svg",
+    bgColor: "bg-[#dfdfe0]/90",
+  },
+  {
+    name: "Spring",
+    description: "Java-based application framework.",
+    imageUrl: "/imgs/logos/spring.svg",
+    bgColor: "bg-[#77bc1f]/40",
+  },
+  {
+    name: "Kotlin",
+    description: "Modern, concise programming language.",
+    imageUrl: "/imgs/logos/kotlin.svg",
+    bgColor: "bg-[#ae27eb]/25",
+  },
+  {
+    name: "Ktor",
+    description: "Asynchronous server framework for Kotlin.",
+    imageUrl: "/imgs/logos/ktor.svg",
+    bgColor: "bg-[#0ba8ff]/20",
+  },
+  {
+    name: "C#",
+    description: "Microsoft's object-oriented language.",
+    imageUrl: "/imgs/logos/csharp.svg",
+    bgColor: "bg-[#9b4f96]/20",
+  },
+  {
+    name: ".NET",
+    description: "Microsoft's cross-platform framework.",
+    imageUrl: "/imgs/logos/dotnetcore.svg",
+    bgColor: "bg-[#623697]/20",
+  },
+  {
+    name: "PostgreSQL",
+    description: "Open-source relational database.",
+    imageUrl: "/imgs/logos/postgresql.svg",
+    bgColor: "bg-[#336791]/20",
+  },
+  {
+    name: "MongoDB",
+    description: "NoSQL document database.",
+    imageUrl: "/imgs/logos/mongodb.svg",
+    bgColor: "bg-[#4e9e44]/20",
+  },
+  {
+    name: "JavaScript",
+    description: "Dynamic web programming language.",
+    imageUrl: "/imgs/logos/javascript.svg",
+    bgColor: "bg-[#f0db4f]/20",
+  },
+  {
+    name: "TypeScript",
+    description: "Typed superset of JavaScript.",
+    imageUrl: "/imgs/logos/typescript.svg",
+    bgColor: "bg-[#3178c6]/20",
+  },
+  {
+    name: "React",
+    description: "JavaScript library.",
+    imageUrl: "/imgs/logos/react.svg",
+    bgColor: "bg-[#61DAFB]/20",
+  },
+  {
+    name: "Tailwind",
+    description: "CSS framework.",
+    imageUrl: "/imgs/logos/tailwind.svg",
+    bgColor: "bg-[#0EA5E9]/20",
+  },
+  {
+    name: "Git",
+    description: "Version control.",
+    imageUrl: "/imgs/logos/git.svg",
+    bgColor: "bg-[#F1502F]/20",
+  },
+  {
+    name: "Swagger",
+    description: "API documentation tool.",
+    imageUrl: "/imgs/logos/swagger.svg",
+    bgColor: "bg-[#85ea2d]/20",
+  },
+];
