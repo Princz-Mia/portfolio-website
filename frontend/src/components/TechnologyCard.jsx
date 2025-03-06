9af7ef003bb17cbb2517a5f0af6d4d2fa5cab9d4
Princz Mia <princz.mia@gmail.com>
2025-03-06 20:45:23 +0100

    feat: add TechnologyCard component to display technologies

---
+++ frontend/src/components/TechnologyCard.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const TechnologyCard = ({ cardInfo }) => {
+  const { name, description, imageUrl, bgColor } = cardInfo;
+
+  return (
+    <div className="flex flex-1 gap-5 p-2.5 rounded-xl border-1 border-black/20 bg-black/30 hover:bg-black/15 hover:border-(--dark400) transition-colors duration-200">
+      <div className={`p-3 ${bgColor} rounded-lg w-fit flex-shrink-0`}>
+        <Image
+          src={imageUrl}
+          width={1000}
+          height={1000}
+          alt={`${name} logo`}
+          className="size-8"
+        />
+      </div>
+      <div className="overflow-clip">
+        <h4 className="text-lg font-medium">{name}</h4>
+        <p className="text-white/70 text-sm ">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TechnologyCard;
