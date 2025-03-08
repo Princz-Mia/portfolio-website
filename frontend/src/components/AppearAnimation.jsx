068ca95c9ae3e1814f28849ac9b9bf8451de66c2
Princz Mia <princz.mia@gmail.com>
2025-03-08 17:37:26 +0100

    feat: add id attribute for navigation purposes

--- frontend/src/components/AppearAnimation.jsx
+++ frontend/src/components/AppearAnimation.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const AppearAnimation = ({ children }) => {
+const AppearAnimation = ({ children, id }) => {
   const panelRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
 
@@ -25,12 +25,14 @@ const AppearAnimation = ({ children }) => {
   }, []);
 
   return (
+    <div id={id} className="md:pt-4 lg:pt-8">
     <div
       ref={panelRef}
-      className={`w-full max-w-sm sm:max-w-lg md:max-w-3xl lg:max-w-4xl xl:max-w-6xl flex mx-auto p-6 md:my-8 mb-6 lg:mt-12 bg-black/20 border-2 border-black/20 rounded shadow-lg transform transition-all duration-500 ease-in-out 
+      className={`w-full max-w-sm sm:max-w-lg md:max-w-3xl lg:max-w-4xl xl:max-w-6xl flex mx-auto p-6 md:my-8 mb-6 bg-black/20 border-2 border-black/20 rounded shadow-lg transform transition-all duration-500 ease-in-out 
         ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
     >
       {children}
+    </div>
     </div>
   );
 }
